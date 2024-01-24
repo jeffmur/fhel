c55bc35c1f369e1427b0a963f6c014bd9e2b17fa
jeffmur <jeffmur@uw.edu>
2024-01-24 09:33:39 -0800

    Update scheme for context tests

--- dart/test/context_gen_test.dart
+++ dart/test/context_gen_test.dart
@@ -3,13 +3,13 @@ import 'package:fhe/fhe.dart' show FHE;
 
 void main() {
   test('Valid Parameters', () {
-    final fhe = FHE("seal");
+    final fhe = FHE.withScheme("seal", "bfv");
     final context = fhe.genContext(4096, 20, 1024, 128);
     expect(context, "success: valid");
   });
 
   test('Invalid Plaintext Mod Degree', () {
-    final fhe = FHE("seal");
+    final fhe = FHE.withScheme("seal", "bfv");
     final context = fhe.genContext(0, 0, 0, 0);
     expect(context, "invalid_argument: non-standard poly_modulus_degree");
   });
@@ -17,7 +17,7 @@ void main() {
   // In Microsoft SEAL the degree of the polynomial modulus must be a power of 2
   // (e.g. 1024, 2048, 4096, 8192, 16384, or 32768).
   test('Invalid Plaintext Mod Bit Count', () {
-    final fhe = FHE("seal");
+    final fhe = FHE.withScheme("seal", "bfv");
     final context = fhe.genContext(4096, 0, 0, 0);
     expect(context.split(':')[0], "invalid_plain_modulus_bit_count");
   });
