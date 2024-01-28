ed697b33973198c5bcb26f1c5d96f9ee08c48dfc
jeffmur <jeffmur@uw.edu>
2024-01-27 18:05:50 -0800

    Test linux

--- dart/test/addition_test.dart
+++ dart/test/addition_test.dart
@@ -3,7 +3,7 @@ import 'package:fhe/fhe.dart' show FHE;
 import 'package:fhe/afhe/plaintext.dart';
 
 void main() {
-  test("Addition", () {
+  test("SEAL Addition", () {
     final fhe = FHE.withScheme('seal', 'bfv');
     String status = fhe.genContext(4096, 20, 1024, 128);
     expect(status, 'success: valid');
@@ -15,5 +15,19 @@ void main() {
     final ct_res = fhe.add(ct_x, ct_add);
     final pt_res = fhe.decrypt(ct_res);
     expect(pt_res.text, '3');
+  });
+
+  test("SEAL Limitation MacOS?", () {
+    final fhe = FHE.withScheme('seal', 'bfv');
+    String status = fhe.genContext(4096, 20, 1024, 128);
+    expect(status, 'success: valid');
+    fhe.genKeys();
+    final pt_x = Plaintext.withValue(fhe.backend, '399');
+    final ct_x = fhe.encrypt(pt_x);
+    final pt_add = Plaintext.withValue(fhe.backend, '1');
+    final ct_add = fhe.encrypt(pt_add);
+    final ct_res = fhe.add(ct_x, ct_add);
+    final pt_res = fhe.decrypt(ct_res);
+    expect(pt_res.text, '400');
   });
 }
