22a9597782fb2f71416c83298dafb9ec296ff362
jeffmur <jeffmur@uw.edu>
2024-02-17 13:46:02 -0800

    Dart test List<int> multiplication

--- dart/test/seal/multiplication_test.dart
+++ dart/test/seal/multiplication_test.dart
@@ -4,14 +4,9 @@ import 'package:fhel/seal.dart' show Seal;
 
 const schemes = ['bgv', 'bfv'];
 
-String multiply(String scheme, String a, String b, Map<String, int> ctx, {bool encryptMultiplier=true}) {
-  final fhe = Seal(scheme);
-  String status = fhe.genContext(ctx);
-  expect(status, 'success: valid');
+Plaintext multiply(Seal fhe, Plaintext pt_x, Plaintext pt_m, {bool encryptMultiplier=true}) {
   fhe.genKeys();
-  final pt_x = fhe.plain(a);
   final ct_x = fhe.encrypt(pt_x);
-  final pt_m = fhe.plain(b);
   Ciphertext ct_res;
   // Relinearization is required for cipher x cipher multiplication
   if (encryptMultiplier) {
@@ -27,8 +22,7 @@ String multiply(String scheme, String a, String b, Map<String, int> ctx, {bool e
     expect(ct_res.size(), 2);
   }
 
-  final pt_res = fhe.decrypt(ct_res);
-  return pt_res.text.toLowerCase();
+  return fhe.decrypt(ct_res);
 }
 
 void main() {
@@ -39,12 +33,39 @@ void main() {
       'secLevel': 128
     };
     for (var sch in schemes) {
+      final fhe = Seal(sch);
+      final pt_hex_10 = fhe.plain(10.toRadixString(16));
+      String status = fhe.genContext(ctx);
+      expect(status, 'success: valid');
       expect(
         100.toRadixString(16),
-        multiply(sch, 10.toRadixString(16), 10.toRadixString(16), ctx));
+        multiply(fhe, pt_hex_10, pt_hex_10).text.toLowerCase());
       expect(
         100.toRadixString(16),
-        multiply(sch, 10.toRadixString(16), 10.toRadixString(16), ctx, encryptMultiplier: false));
+        multiply(fhe, pt_hex_10, pt_hex_10, encryptMultiplier: false).text.toLowerCase());
+    }
+  });
+
+  test("List<int> Multiplication", () {
+    Map<String, int> ctx = {
+      'polyModDegree': 8192,
+      'ptModBit': 20,
+      'secLevel': 128
+    };
+    List<int> x = [1, 2, 3, 4];
+    List<int> m = [2, 4, 6, 8];
+    List<int> product = [2, 8, 18, 32];
+    int arr_len = product.length;
+
+    for (var sch in schemes) {
+      final fhe = Seal(sch);
+      String status = fhe.genContext(ctx);
+      expect(status, 'success: valid');
+      final pt_x = fhe.encodeVecInt(x);
+      final pt_m = fhe.encodeVecInt(m);
+
+      expect(product, fhe.decodeVecInt(multiply(fhe, pt_x, pt_m), arr_len));
+      expect(product, fhe.decodeVecInt(multiply(fhe, pt_x, pt_m, encryptMultiplier: false), arr_len));
     }
   });
 }
