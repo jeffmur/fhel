e6165259df58d58e7439a848d4e950fa04f10b38
jeffmur <jeffmur@uw.edu>
2024-03-28 21:09:17 -0700

    multiplication demo

--- example/lib/multiplication.dart
+++ example/lib/multiplication.dart
@@ -40,15 +40,15 @@ String multiplyAsHex(SessionChanges s, int x, int mult, bool xEncrypted, bool mu
     s.logSession();
     final start = DateTime.now();
     String xRadix = x.toRadixString(16);
-    s.log('Hex: $x -> $xRadix');
+    s.log('$x.toRadixString(16) => $xRadix');
     Plaintext plainX = fhe.plain(xRadix);
 
     String multRadix = mult.toRadixString(16);
-    s.log('Hex: $mult -> $multRadix');
+    s.log('$mult.toRadixString(16) => $multRadix');
     Plaintext plainMult = fhe.plain(multRadix);
 
     if (!xEncrypted && !multEncrypted) {
-      s.log('Multiplying $x and $mult');
+      s.log('Multiplying Plaintext x Plaintext');
       final result = (x * mult).toString();
       s.log('Result: $result');
       s.log('Elapsed: ${DateTime.now().difference(start).inMilliseconds} ms');
@@ -58,7 +58,7 @@ String multiplyAsHex(SessionChanges s, int x, int mult, bool xEncrypted, bool mu
 
     final plainResult = fhe.decrypt(cipherResult);
     final result = int.parse(plainResult.text, radix: 16).toString();
-    s.log('Result: $result');
+    s.log('Decrypted result: $result');
     s.log('Elapsed: ${DateTime.now().difference(start).inMilliseconds} ms');
     return result.toString();
   } catch (e) {
@@ -85,8 +85,9 @@ String multiplyDouble(SessionChanges s, double x, double mult, bool xEncrypted, 
     Plaintext plainMult = fhe.encodeDouble(mult);
 
     if (!xEncrypted && !multEncrypted) {
-      s.log('Multiplying $x and $mult');
+      s.log('Multiplying Plaintext x Plaintext');
       final result = (x * mult).toString();
+      s.log('Result: $result');
       s.log('Elapsed: ${DateTime.now().difference(start).inMilliseconds} ms');
       return result;
     }
@@ -94,7 +95,7 @@ String multiplyDouble(SessionChanges s, double x, double mult, bool xEncrypted, 
 
     final plainResult = fhe.decrypt(cipherResult);
     final result = fhe.decodeVecDouble(plainResult, 1)[0];
-    s.log('Result: $result');
+    s.log('Decrypted result: $result');
     s.log('Elapsed: ${DateTime.now().difference(start).inMilliseconds} ms');
     return result.toString();
   } catch (e) {
@@ -117,11 +118,12 @@ String multiplyVecInt(SessionChanges s, List<int> x, List<int> mult, bool xEncry
     final plainMult = fhe.encodeVecInt(mult);
 
     if (!xEncrypted && !multEncrypted) {
-      s.log('Multiplying $x x $mult');
+      s.log('Multiplying Plaintext x Plaintext');
       final result = [];
       for (int i = 0; i < x.length; i++) {
         result.add(x[i] * mult[i]);
       }
+      s.log('Result: $result');
       s.log('Elapsed: ${DateTime.now().difference(start).inMilliseconds} ms');
       return result.join(',');
     }
@@ -129,7 +131,7 @@ String multiplyVecInt(SessionChanges s, List<int> x, List<int> mult, bool xEncry
 
     final plainResult = fhe.decrypt(cipherResult);
     final result = fhe.decodeVecInt(plainResult, x.length);
-    s.log('Result: $result');
+    s.log('Decrypted result: $result');
     s.log('Elapsed: ${DateTime.now().difference(start).inMilliseconds} ms');
     return result.join(',');
   } catch (e) {
@@ -161,6 +163,7 @@ String multiplyVecDouble(SessionChanges s, List<double> x, List<double> mult, bo
       for (int i = 0; i < x.length; i++) {
         result.add(x[i] * mult[i]);
       }
+      s.log('Result: $result');
       s.log('Elapsed: ${DateTime.now().difference(start).inMilliseconds} ms');
       return result.join(',');
     }
@@ -168,7 +171,7 @@ String multiplyVecDouble(SessionChanges s, List<double> x, List<double> mult, bo
 
     final plainResult = fhe.decrypt(cipherResult);
     final result = fhe.decodeVecDouble(plainResult, x.length);
-    s.log('Result: $result');
+    s.log('Decrypted result: $result');
     s.log('Elapsed: ${DateTime.now().difference(start).inMilliseconds} ms');
     return result.join(',');
   } catch (e) {
